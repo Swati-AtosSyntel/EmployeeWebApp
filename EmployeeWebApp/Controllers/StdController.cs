44da8046f8a26bd7bfd8561bfe0eb1a7ad3e1a61
nbhir <nbhir@DESKTOP-RKMH2OS>
2022-02-25 17:36:59 +0530

    Add project files.

---
+++ EmployeeWebApp/Controllers/StdController.cs
@@ -0,0 +1,96 @@
+﻿using EmployeeWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeWebApp.Controllers
+{
+    public class StdController : Controller
+    {
+        // GET: Std
+        public ActionResult Index()
+        {
+            List<Student> students = new List<Student>()
+            {
+                new Student(){sid=1,sname="Swati"},
+                new Student(){sid=2,sname="Nishant"},
+                new Student(){sid=3,sname="Bhushan"}
+            };
+            return View(students);
+        }
+
+        // GET: Std/Details/5
+        public ActionResult Details(int? id)
+        {
+            return View();
+        }
+
+        // GET: Std/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Std/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Std/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            return View();
+        }
+
+        // POST: Std/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int? id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Std/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            return View();
+        }
+
+        // POST: Std/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int? id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ EmployeeWebApp/Controllers/StudentController.cs
@@ -0,0 +1,32 @@
+﻿using EmployeeWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeWebApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index(int id)
+        {
+            //to pass any data from controller to view , use Viewbag
+            //viewbag is a dynamic object introduced in .net framework 4.0
+            //
+            ViewBag.data = id;
+            //ViewData is dictionary object and can be used to pass data from controller to view
+            ViewData["ID"] = id;
+
+            return View();
+        }
+        public ActionResult StudentDetails()
+        {
+            Student s = new Student() { sid = 1, sname = "Shivani" };
+            ViewBag.std = s;
+            ViewData["std"] = s;
+            return View();
+        }
+    }
+}
